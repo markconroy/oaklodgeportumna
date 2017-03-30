6fdecedce600d6ae60bc549249b61fa62ca8bb00
Mark Conroy <info@adesignforlife.net>
2017-03-30 18:02:33 +0100

    mostly styling for the footer, and tidying up a few things

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+  <div class="container">
+    <div class="inner">
+      <p>Get in contact with us any time</p>
+      <p>Landline: +353 (0)90 974 1549<br>Mobile: 087 7924161<br><a href="mailto:oaklodgeportumna@gmail.com">oaklodgeportumna@gmail.com</a></p>
+      <p>St Brendan's Road,<br>Portumna,<br>Co Galway,<br>Republic of Ireland</p>
+    </div>
+      <div class="inner social">
+        <img src="/assets/images/free_wi_fi_spot.gif" alt="Free Wi-Fi for all Guests" />
+        <a href="https://www.facebook.com/oaklodgeportumna"><img src="/assets/images/find-us-on-facebook.jpg" alt="Oak Lodge Portumna Facebook Logo" /></a>
+      </div>
+  </div>
+</footer>
+<?php include 'includes/closing.php'; ?>
