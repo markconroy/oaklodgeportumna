396ed20cf2c2e76ae566e7952e9ed6f96238b4e7
Mark Conroy <info@adesignforlife.net>
2017-03-30 21:04:42 +0100

    adding stub pages

--- includes/menu.php
+++ includes/menu.php
@@ -1,11 +1,11 @@
 <nav class="nav">
-  <button class="menu-toggle">Menu</button>
+  <button class="menu-toggle small-medium-screen-only">Menu</button>
   <ul class="main-menu">
     <li class="menu-item"><a href="/">Home</a></li>
-    <li class="menu-item"><a href="/bed-breakfast/about-us">About Us</a></li>
-    <li class="menu-item"><a href="/bed-breakfast/facilities">Facilities</a></li>
-    <li class="menu-item"><a href="/bed-breakfast/room-rates">Rooms and Rates</a></li>
-    <li class="menu-item"><a href="/bed-breakfast/fishing-holidays-ireland">Fishing</a></li>
+    <li class="menu-item"><a href="/about-us">About Us</a></li>
+    <li class="menu-item"><a href="/facilities">Facilities</a></li>
+    <li class="menu-item"><a href="/rooms-rates">Rooms and Rates</a></li>
+    <li class="menu-item"><a href="/fishing-holidays-ireland">Fishing</a></li>
     <li class="menu-item"><a href="/local-attractions">Local Attractions</a></li>
     <li class="menu-item"><a href="/contact-us">Contact Us</a></li>
   </ul>
