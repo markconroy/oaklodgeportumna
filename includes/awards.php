f911264e4d1e5083b98535d9a5d12ff5d6f45f4e
Mark Conroy <info@adesignforlife.net>
2017-03-30 20:20:45 +0100

    layout for awards items

--- includes/awards.php
+++ includes/awards.php
@@ -2,5 +2,7 @@
   <?php include 'tripadvisor/certificate-of-excellence/2014.php'; ?>
   <?php include 'tripadvisor/certificate-of-excellence/2015.php'; ?>
   <?php include 'tripadvisor/certificate-of-excellence/2016.php'; ?>
-  <img class="awards--image" src="/assets/images/4_star_approved.jpg" alt="Fáilte Ireland logo - Oak Lodge Portumna" />
+  <div class="awards--image">
+    <img src="/assets/images/4_star_approved.jpg" alt="Fáilte Ireland logo - Oak Lodge Portumna" />
+  </div>
 </div>
--- includes/meta.php
+++ includes/meta.php
@@ -8,6 +8,5 @@
     <script type="text/javascript" src="/js/libraries/jquery-2.2.4.min.js"></script>
     <script type="text/javascript" src="/js/libraries/jquery.flexslider-min.js"></script>
     <script type="text/javascript" src="/js/script.js"></script>
-
   </head>
   <body>
