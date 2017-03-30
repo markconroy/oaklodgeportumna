202d23ddbf0dd3782d16ce81d169b9f9c7ed4a0e
Mark Conroy <info@adesignforlife.net>
2017-03-30 20:12:59 +0100

    adding viewport meta tag

--- includes/meta.php
+++ includes/meta.php
@@ -3,6 +3,7 @@
   <head>
     <title>Oak Lodge Bed and Breakfast, Portumna, Co. Galway | 5 Stars on TripAdvisor</title>
     <meta name="description" content="Oak Lodge Bed and Breakfast, constantly rated 5 Stars on TripAdvisor, Portumna's only Bord Fáilte approved four star B&B." />
+    <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no">
     <link type="text/css" rel="stylesheet" href="/css/style.css" media="all" />
     <script type="text/javascript" src="/js/libraries/jquery-2.2.4.min.js"></script>
     <script type="text/javascript" src="/js/libraries/jquery.flexslider-min.js"></script>
