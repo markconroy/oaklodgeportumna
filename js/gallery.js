df69ed43874b07916326f9ba9538b143972b662f
Mark Conroy <info@adesignforlife.net>
2017-04-07 21:38:26 +0100

    rewriting the gallery, much simpler and works much better

--- js/gallery.js
+++ js/gallery.js
@@ -2,34 +2,24 @@ $(document).ready(function() {
 
   // Code for our very simple gallery.
 
-  $('.gallery--popup-item').hide();
+  function gallery() {
 
-  $('.gallery--button').click(function() {
-    $('.gallery--popup').show();
-  });
+    var $galleryButtonOpen = $('.gallery--button');
+    var $galleryButtonClose = $('.gallery--popup--close');
+    var $galleryItemWrapper = $('.gallery--item');
+    var $galleryImage = $('.gallery--popup-item')
 
-  $('.gallery--popup--close').click(function() {
-    $('.gallery--popup').hide();
-    $('.gallery--popup--item').hide();
-  });
+    $galleryButtonOpen.each(function() {
+      $(this).click(function() {
+        $(this).closest($galleryItemWrapper).find($galleryImage).fadeIn();
+      });
+    });
 
-  $('.gallery--button--1').click(function() {
-    $('.gallery-popup--item--1').show();
-  });
-  $('.gallery--button--2').click(function() {
-    $('.gallery-popup--item--2').show();
-  });
-  $('.gallery--button--3').click(function() {
-    $('.gallery-popup--item--3').show();
-  });
-  $('.gallery--button--4').click(function() {
-    $('.gallery-popup--item--4').show();
-  });
-  $('.gallery--button--5').click(function() {
-    $('.gallery-popup--item--5').show();
-  });
-  $('.gallery--button--6').click(function() {
-    $('.gallery-popup--item--6').show();
-  });
+    $galleryButtonClose.click(function() {
+      $galleryImage.fadeOut();
+    });
+  }
+
+  gallery();
 
 });
