153ce875496a52fdf77c458c2cc12ae966824a21
Mark Conroy <info@adesignforlife.net>
2017-04-15 14:46:56 +0100

    tweaks for awards styling

--- includes/awards.php
+++ includes/awards.php
@@ -1,7 +1,13 @@
 <div class="awards">
-  <?php include 'tripadvisor/certificate-of-excellence/2014.php'; ?>
-  <?php include 'tripadvisor/certificate-of-excellence/2015.php'; ?>
-  <?php include 'tripadvisor/certificate-of-excellence/2016.php'; ?>
+  <div class="award">
+    <?php include 'tripadvisor/certificate-of-excellence/2014.php'; ?>
+  </div>
+  <div class="award">
+    <?php include 'tripadvisor/certificate-of-excellence/2015.php'; ?>
+  </div>
+  <div class="award">
+    <?php include 'tripadvisor/certificate-of-excellence/2016.php'; ?>
+  </div>
   <div class="awards--image">
     <img src="/assets/images/4_star_approved.jpg" alt="Fáilte Ireland logo - Oak Lodge Portumna" />
   </div>
