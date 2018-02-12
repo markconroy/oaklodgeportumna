b3fc5a424b6a84702db2059b472b998139f3f52e
Mark Conroy <info@adesignforlife.net>
2018-02-12 14:00:01 +0000

    Updating rates

--- rooms-rates/index.php
+++ rooms-rates/index.php
@@ -7,8 +7,8 @@
   <div class="container">
     <h1>Rooms and Rates</h1>
     <p>Pricing at <a href="/">Oak Lodge Portumna</a> depends on the number of nights you wish to stay and the type of room you wish to avail of. We have five rooms available, all are <strong>ensuite rooms</strong>.</p>
-    <p>Prices range from €25 - €40 per person sharing (pps).</p>
-    <p>We also have "<strong>room only</strong>" rates for people who wish to opt for <strong>self-catering accommodation in Portumna</strong>, rather than <strong>bed &amp; breakfast</strong>. Room only rates are €20 pps.</p>
+    <p>Prices €40 per person sharing (pps).</p>
+    <p>Single Rate: €50</p>
     <?php include '../includes/awards.php'; ?>
   </div>
 </main>
